6eb38088941a77982cb12a21976dadea4e4548c6
Gabriel Lacerda <gl547842@gmail.com>
2025-08-28 16:49:31 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,30 @@
 package Usuario;
 
+import Usuario.Catalogo;
+
+
+
 public class Main {
 
-	public static void main(String[] args) {
-	
+    public static void main(String[] args) {
+        Catalogo catalogo = new Catalogo();
 
-	}
+        Midia m1 = new Musica("Imagine", "John Lennon", 180, Genero.ROCK);
+        Midia m2 = new Musica("Águas de Março", "Tom Jobim", 200, Genero.MPB);
 
+        catalogo.adicionarMidia(m1);
+        catalogo.adicionarMidia(m2);
+
+        Usuario user = new Usuario("Ana", "ana@email.com");
+        user.criarPlaylist("Favoritas");
+
+        Playlist favoritas = user.getPlaylist("Favoritas");
+
+        favoritas.adicionarMusica(m1);
+        favoritas.adicionarMusica(m2);
+
+        favoritas.visualizarPlaylist(catalogo);
+
+        System.out.println("Duração total: " + favoritas.calcularDuracaoTotal(catalogo) + " segundos");
+    }
 }
