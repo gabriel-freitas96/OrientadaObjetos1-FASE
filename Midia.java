252bf17fdbabfd10581c741c5a33e9e1a6012f16
Gabriel Lacerda <gl547842@gmail.com>
2025-08-28 16:50:15 -0300

    Update Midia.java

--- Midia.java
+++ Midia.java
@@ -1,41 +1,67 @@
 package Usuario;
 
-public abstract class Midia {
+public class Midia {
 	protected String titulo;
 	protected String artista;
 	protected int duracaoSegudos;
 	protected Genero genero;
+	private String tipo;
 	
-	public  Midia(String titulo,String artista,int duracaoSegudos,Genero genero) {
+	public  Midia(String titulo,String artista,int duracaoSegudos,Genero genero,String tipo) {
 		this.titulo=titulo;
 		this.artista=artista;
 		this.duracaoSegudos=duracaoSegudos;
 		this.genero=genero;
+		this.tipo = tipo;
 	}
-
-
 
 	public String getTitulo() {
 		return titulo;
 	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
 	public String getArtista() {
 		return artista;
 	}
+
+	public void setArtista(String artista) {
+		this.artista = artista;
+	}
+
 	public int getDuracaoSegudos() {
 		return duracaoSegudos;
 	}
+
+	public void setDuracaoSegudos(int duracaoSegudos) {
+		this.duracaoSegudos = duracaoSegudos;
+	}
+
 	public Genero getGenero() {
 		return genero;
 	}
 
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
 
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 
 	@Override
 	public String toString() {
 		return "Midia [titulo=" + titulo + ", artista=" + artista + ", duracaoSegudos=" + duracaoSegudos + ", genero="
-				+ genero + "]";
+				+ genero + ", tipo=" + tipo + "]";
 	}
 }
+
 
 
 
