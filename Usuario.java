761c7f26c6e39e4790571cee2a7a9d490ef14bc5
Gabriel Lacerda <gl547842@gmail.com>
2025-08-28 16:53:13 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,5 +1,54 @@
 package Usuario;
 
-public class Usuario {
+import java.util.ArrayList;
+import java.util.List;
 
-}
+public class Usuario {
+	protected String nome;
+	protected String email;
+	protected ArrayList<Playlist> playlist;
+	
+	public Usuario(String nome, String email) {
+		this.nome=nome;
+		this.email=email;
+		this.playlist = new ArrayList<>();
+	  }
+
+    public void criarPlaylist(String nomePlaylist) {
+    	Playlist nova = new Playlist(nomePlaylist, this);
+        playlist.add(nova);
+    }
+
+    public Playlist getPlaylist(String nome) {
+        for (Playlist p : playlist) {
+            if (p.getNome().equalsIgnoreCase(nome)) {
+                return p;
+            }
+        }
+        return null;
+    }
+    public ArrayList<Playlist> getPlaylists() {
+        return playlist;
+    }
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Playlist> getPlaylist() {
+		return playlist;
+	}
+
+	}
