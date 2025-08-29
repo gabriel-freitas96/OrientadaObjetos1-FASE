507dd0d8d537c4201181ee4d7b7a1c12decd8cfc
Samires do Carmo <samiresdocarmo@gmail.com>
2025-08-28 21:21:42 -0300

    Update and rename Genero.java to GeneroMusical.java

--- Genero.java
+++
@@ -1,5 +0,0 @@
-package Usuario;
-
-public enum Genero {
-	 ROCK, POP, MPB, JAZZ, CLASSICA, ELETRONICA, RAP, BLUES, COUNTRY
-}
---
+++ GeneroMusical.java
@@ -0,0 +1,5 @@
+package www.br.com.unifacisa;
+
+public enum GeneroMusical {
+	POP,ROCK,MPB,CLASSICA,RAP
+	}
