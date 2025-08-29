8fadf7c395e72c943bd17da6ae95b9459170c375
Samires do Carmo <samiresdocarmo@gmail.com>
2025-08-28 21:22:11 -0300

    Update Catalogo.java

--- Catalogo.java
+++ Catalogo.java
@@ -1,48 +1,62 @@
-package Usuario;
+package www.br.com.unifacisa;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 public class Catalogo {
-	private ArrayList<Midia> midias;
-	
-	public Catalogo() {
-		this.midias = new ArrayList<>();
-	}
-	
-	public void adicionarMidia(Midia midia) {
-		midias.add(midia);
-	}
-	public Midia buscarPorTitulo(String titulo) {
-	    for (Midia musicas : midias) {
-	    	 if (musicas.getTitulo().equalsIgnoreCase(titulo)) { 
-	            return musicas;
-	        }
-	    }
-	    return null;
-	}
-	public List<Midia>buscarPorArtista(String Titulo){
-		List<Midia>resultado=new ArrayList<>();
-		for(Midia m:midias) {
-			if(m.getArtista().equalsIgnoreCase(Titulo)) {
-				resultado.add(m);
-			}
-		}
-		return resultado;
-	}
-	public List<Midia>buscarPorGenero(Genero genero){
-		List<Midia>resultado=new ArrayList<>();
-		for(Midia m:midias) {
-			if(m.getGenero()==genero) {
-				resultado.add(m);
-			}
-		}
-		return resultado;
-	}
-	public ArrayList<Midia> getMidias() {
-		return midias;
-	}
-	
-	
+  private ArrayList<Midia>midias = new ArrayList<>();
 
+  public Catalogo(ArrayList<Midia> midias) {
+	  super();
+	  this.midias = midias;
+ 	}
+  
+  public Catalogo() {
+	  
+  }
+
+  public ArrayList<Midia> getMidias() {
+	  return midias;
+  	}
+
+  public void setMidias(ArrayList<Midia> midias) {
+	  this.midias = midias;
+  	}
+  
+  public void adicionarMidias(Midia playlist){
+	  midias.add(playlist);
+  }
+
+  @Override
+  public int hashCode() {
+	return Objects.hash(midias);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Catalogo other = (Catalogo) obj;
+	return Objects.equals(midias, other.midias);
+  }
+
+  @Override
+  public String toString() {
+	return "Catalogo [midias=" + midias + "]";
+  }
+
+  public void adicionarMidia(Midia midia) {
+	  midias.add(midia);
+  }
+  
+  
+
+  
+	
+  
+ 
 }
