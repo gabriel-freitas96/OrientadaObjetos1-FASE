2e59c2b42a3f849d8f62edd773002b3739eadba8
Samires do Carmo <samiresdocarmo@gmail.com>
2025-08-28 21:24:30 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,54 +1,59 @@
-package Usuario;
+package www.br.com.unifacisa;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 public class Usuario {
-	protected String nome;
-	protected String email;
-	protected ArrayList<Playlist> playlist;
+	private String nome;
+	private String email;
 	
 	public Usuario(String nome, String email) {
-		this.nome=nome;
-		this.email=email;
-		this.playlist = new ArrayList<>();
-	  }
-
-    public void criarPlaylist(String nomePlaylist) {
-    	Playlist nova = new Playlist(nomePlaylist, this);
-        playlist.add(nova);
-    }
-
-    public Playlist getPlaylist(String nome) {
-        for (Playlist p : playlist) {
-            if (p.getNome().equalsIgnoreCase(nome)) {
-                return p;
-            }
-        }
-        return null;
-    }
-    public ArrayList<Playlist> getPlaylists() {
-        return playlist;
-    }
-
+		super();
+		this.nome = nome;
+		this.email = email;
+		
+	}
+	
+	public Usuario() {
+		
+	}
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-	public List<Playlist> getPlaylist() {
-		return playlist;
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, nome);
 	}
-
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(email, other.email) && Objects.equals(nome, other.nome);
+				
 	}
+	
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Usuario [nome=" + nome + ", email=" + email + "]";
+	}
+}
+
