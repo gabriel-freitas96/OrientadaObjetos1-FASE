b1a0656a8a46228fae9ae4b46079691f2ec07472
Gabriel Lacerda <gl547842@gmail.com>
2025-09-01 15:48:21 -0300

    Update Musica.java

--- Musica.java
+++ Musica.java
@@ -1,21 +1,21 @@
+package Usuario;
+
 public class Musica extends Midia{
 	private GeneroMusical genero;
 	
 	public Musica(String titulo, String artista, double duracao) {
 		super(titulo,artista,duracao);
-		this.genero = genero;
-		
 	}
-	
 	public Musica() {
 	}
-
 	public GeneroMusical getGenero() {
 		return genero;
 	}
-
 	public void setGenero(GeneroMusical genero) {
 		this.genero = genero;
 	}
-	
+	@Override
+	public String toString() {
+		return "Musica [genero=" + genero + "]";
+	}
 }
